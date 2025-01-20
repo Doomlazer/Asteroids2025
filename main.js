e233e46903ab59039293ad54692f59cdd1dde6d6
DL <dl@thespr3.com>
2025-01-20 10:20:00 -0800

    tradingpost scales

--- main.js
+++ main.js
@@ -34,17 +34,17 @@ let resourceNames = ['Gold', 'Silver', 'Copper', 'Diamond', 'Platnum', 'Uranium'
 let resourceColor = ['#E8B923', '#7d3c98', '#3498db', '#27ae60', '#d35400', '#5d6d7e', '#cd6155', '#48c9b0'];
 // x is 1.724% y is 3.06
 let constellationStarsArrays = [[2.9, 12.9, 4.5, 7.6, 6.2, 7.7, 6.4, 11.8, 6.0, 12.9, 6.9, 6.2, 8.5, 5.3, 10.0, 4.7, 9.1, 8.8, 9.4, 11.0, 11.8, 6.9, 14.7, 9.4],
-                                [13.1, 9.6, 11.5, 9.7, 7.0, 11.6, 4.3, 11.7, 3.2, 12.6, 2.9, 11.7, 2.4, 11.6, 13.9, 11.6, 14.0, 12.8, 7.2, 5.9, 5.9, 5.5],
-                                [7.1, 3.7, 8.8, 9.3, 8.9, 11.6, 8.3, 15.5, 12.4, 14.2, 11.6, 8.9],
-                                [12.5, 5.8, 12.2, 7.5, 9.2, 15.2, 8.9, 15.9, 5.2, 11.2, 4.7, 9.5, 4.3, 9.2, 3.9, 8.2, 4.8, 8.4, 8.1, 8.6, 8.2, 10.5],
-                                [11.3, 14.9, 9.4, 11.9, 11.8, 12.3, 7.0, 10.8, 5.0, 8.8, 3.3, 6.8, 3.8, 10.1, 5.9, 7.7, 6.7, 5.6, 4.2, 5.1, 7.5, 3.5, 8.6, 8.3, 11.1, 10.0, 11.2, 7.7, 12.6, 7.5, 14.3, 5.4],
-                                [2.9, 10.4, 5.4, 8.2, 5.4, 10.3, 4.7, 12.0, 4.9, 14.9, 7.4, 13.3, 9.6, 10.0, 9.4, 12.1, 11.9, 13.2, 9.0, 8.2, 9.2, 6.9, 11.4, 5.4, 14.1, 5.1, 13.8, 8.0, 12.2, 7.3],
-                                [5.1, 6.1, 3.0, 7.9, 4.2, 10.4, 5.9, 9.8, 6.7, 8.9, 8.7, 3.8, 11.1, 8.5, 9.7, 14.1, 5.9, 15.2, 5.2, 15.4, 4.9, 16.4, 10.5, 3.5],
-                                [13.1, 14.4, 11.8, 14.8, 11.3, 13.3, 10.3, 13.9, 9.0, 13.2, 6.0, 12.9, 4.7, 12.7, 2.0, 14.6, 3.3, 9.9, 4.6, 6.8, 5.8, 5.6, 5.9, 4.4, 5.5, 3.7, 6.5, 2.8],
-                                [5.7, 16.1, 6.3, 15.0, 3.4, 14.7, 5.8, 12.8, 2.6, 10.9, 2.5, 9.9, 1.9, 6.9, 2.9, 6.2, 4.7, 4.6, 7.1, 7.3, 7.3, 9.4, 9.6, 7.6, 8.1, 6.5, 7.6, 4.9, 8.2, 4.5, 7.2, 3.7, 6.7, 3.0, 6.0, 2.6, 11.5, 7.6, 13.2, 4.8, 12.4, 9.9, 13.7, 10.5, 14.9, 9.3, 10.8, 13.0, 11.7, 14.5],
-                                [11.0, 3.1, 10.1, 6.8, 11.0, 6.8, 11.7, 9.2, 11.5, 11.3, 11.9, 12.6, 9.5, 10.2, 8.5, 10.7, 7.8, 11.7, 7.4, 14.1, 7.5, 15.4, 7.5, 16.6, 6.1, 16.6, 3.8, 16.0, 2.0, 13.8, 2.6, 12.4, 3.1, 10.2, 4.5, 10.4],
-                                [10.1, 7.6, 3.5, 4.0, 6.3, 7.7, 7.4, 9.8, 8.2, 11.4, 8.9, 12.6, 6.9, 12.8, 5.7, 12.1, 1.3, 8.4, 10.3, 13.3, 8.3, 14.7, 10.2, 15.9, 12.8, 12.3, 13.7, 14.3, 14.5, 15.3],
-                                [2.4, 6.6, 5.6, 6.5, 7, 8.3, 9.6, 6.8, 12.7, 5.6, 14.7, 8.6, 14.9, 12.6, 11.9, 12.8, 9.8, 13.5, 7.9, 13.2, 5.8, 13, 3.3, 12.4, 3.2, 10.3, 8.5, 3.3]];
+                                [13.1, 9.6, 11.5, 9.7, 7, 10.6, 4.3, 9.7, 5.2, 13.6, 1.9, 11.7, 2.4, 9.6, 13.9, 11.6, 17, 12.8, 8.2, 5.9, 5.9, 4.5],
+                                [7.1, 3.7, 8.8, 7.3, 8.9, 10.6, 6.3, 13.5, 13.4, 13.2, 11.6, 8.9],
+                                [15.5, 5.8, 13.2, 7.5, 10.2, 13.2, 7.9, 13.9, 5.2, 12.2, 4.7, 9.5, 2.3, 8.2, 1.9, 4.2, 4.8, 6.4, 9.1, 7.6, 9.2, 10.5], // cap
+                                [11.3, 12.9, 9.4, 10.9, 13.8, 12.3, 6, 11.8, 4, 9.8, 1.3, 6.8, 2.8, 11.1, 5.9, 7.7, 6.7, 5.6, 3.2, 5.1, 7.5, 3.5, 8.6, 8.3, 12.1, 10, 12.2, 7.7, 13.6, 6.5, 15.3, 4.4], // gemini
+                                [2.9, 9.4, 5.4, 6.2, 5.4, 9.3, 4.7, 11, 3.9, 13.9, 7.4, 12.3, 9.6, 10, 10.4, 12.1, 13.9, 13.2, 9, 7.2, 9.2, 5.9, 11.4, 4.4, 14.1, 4.1, 13.8, 8, 12.2, 7.3], // leo
+                                [5.1, 3.1, 2, 5.9, 4.2, 9.4, 7.9, 7.8, 6.7, 5.9, 12.7, 4.8, 12.1, 8.5, 9.7, 13.1, 5.9, 11.2, 5.2, 12.4, 3.9, 13.4, 14.5, 3.], // Libra
+                                [13.1, 13.4, 16.8, 13.8, 14.3, 12.3, 11.3, 12.9, 10, 10.2, 7, 12.9, 4.7, 11.7, 2, 13.6, 3.3, 8.9, 4.6, 6.8, 8.8, 6.6, 5.9, 4.4, 7.5, 3.7, 8.5, 2.8], // pisces
+                                [3.7, 13.1, 8.3, 13, 1.4, 13.7, 5.8, 12.8, 2.6, 10.9, 2.5, 8.9, 1.9, 6.9, 2.9, 6.2, 3.7, 4.6, 7.1, 8.3, 7.3, 10.4, 9.6, 9.6, 8.1, 7.5, 7.6, 5.9, 10.2, 6.5, 7.2, 4.7, 6.7, 3, 5, 3.6, 12.5, 7.6, 15.2, 4.8, 14.4, 8.9, 15.7, 10.5, 16.9, 9.3, 12.8, 12, 14.7, 13.5], // sag
+                                [11, 3.1, 10.1, 5.8, 13, 5.8, 11.7, 8.2, 11.5, 10.3, 11.9, 12.6, 7.5, 6.2, 6.5, 7.7, 7.8, 9.7, 6.4, 11.1, 8.5, 11.4, 8.5, 13.6, 6.1, 13.6, 3.8, 13, 2, 13.8, 2.6, 11.4, 2.1, 8.2, 4.5, 8.4], // scorp
+                                [11.1, 5.6, 2.5, 3, 6.3, 4.7, 7.4, 7.8, 10.2, 8.4, 8.9, 10.6, 6.9, 10.8, 3.7, 9.1, 1.3, 7.4, 10.3, 12.3, 8.3, 12.7, 10.2, 13.9, 12.8, 10.3, 14.7, 12.3, 16.5, 13.3], // taurus
+                                [2.4, 6.6, 5.6, 6.5, 7, 8.3, 9.6, 6.8, 12.7, 5.6, 14.7, 8.6, 14.9, 12.6, 11.9, 12.8, 9.8, 13.5, 7.9, 13.2, 5.8, 13, 3.3, 12.4, 3.2, 10.3, 8.5, 3.3]]; // virgo
 let resources = [];
 let deadResources = [];
 let bullets = [];
@@ -320,8 +320,8 @@ class Bullet {
             this.color = '#FFFFFF';
         }
         this.speed = speed;
-        this.hbOffset1 = (e.baseDamage/4);
-        this.hbOffset2 = (e.baseDamage/4)*2;
+        this.hbOffset1 = -this.damage/3;
+        this.hbOffset2 = this.damage/1.5;
     }
 }
 
@@ -405,8 +405,6 @@ class Asteroid {
 // ********************
 // animation & movement
 // ********************
-
-requestAnimationFrame(step);
 
 function step(timeStamp) {
     if (start === undefined) {
@@ -976,60 +974,61 @@ function drawTradingPost() {
     let station = mapLocations[constellation][curMapLoc];
 
     ctx.fillStyle = 'rgb(20, 20, 20)';
-    ctx.fillRect((c.width/10)*2, (c.height/10)*2, (c.width/10)*6, (c.height/10)*6);
+    ctx.fillRect(c.width/20*2, c.height/20*2, c.width/20*16, c.height/20*16);
     ctx.strokeStyle = '#ffffff';
-    ctx.strokeRect((c.width/10)*2, (c.height/10)*2, (c.width/10)*6, (c.height/10)*6);
-    ctx.font = "20px Hyperspace";
+    ctx.strokeRect(c.width/20*2, c.height/20*2, c.width/20*16, c.height/20*16);
+    ctx.font = scaleFont(0.025, "Hyperspace");
     ctx.fillStyle = '#FFFFFF';
-    ctx.fillText("TRADING POST: ", (c.width/10)*2+20, (c.height/10)*2+40);
+    ctx.fillText("TRADING POST :", c.width/20*3, c.height/20*3);
     if (station.buySell == 0) {
         ctx.fillStyle = '#FFFFFF';
-        ctx.fillRect((c.width/10)*2+290, (c.height/10)*2+20, 70, 30);
+        ctx.fillRect(c.width/20*9.8, c.height/20*2.2, c.width/20*1.5, c.height/20*1);
         ctx.fillStyle = '#000000';
-        ctx.fillText("SELL", (c.width/10)*2+305, (c.height/10)*2+40);
+        ctx.fillText("SELL", c.width/20*10, c.height/20*3);
         ctx.fillStyle = '#FFFFFF';
-        ctx.fillText("BUY", (c.width/10)*2+410, (c.height/10)*2+40);
+        ctx.fillText("BUY", c.width/20*14, c.height/20*3);
     } else {
         ctx.fillStyle = '#FFFFFF';
-        ctx.fillRect((c.width/10)*2+390, (c.height/10)*2+20, 70, 30);
+        ctx.fillRect(c.width/20*13.65, c.height/20*2.2, c.width/20*1.5, c.height/20*1);
         ctx.fillStyle = '#FFFFFF';
-        ctx.fillText("SELL", (c.width/10)*2+305, (c.height/10)*2+40);
+        ctx.fillText("SELL", c.width/20*10, c.height/20*3);
         ctx.fillStyle = '#000000';
-        ctx.fillText("BUY", (c.width/10)*2+410, (c.height/10)*2+40);
+        ctx.fillText("BUY", c.width/20*14, c.height/20*3);
     }
 
     ctx.fillStyle = '#FFFFFF';
-    ctx.fillText("PRICE", (c.width/10)*2+275, (c.height/10)*2+100);
-    ctx.fillText("OWNED", (c.width/10)*2+430, (c.height/10)*2+100);
+    ctx.fillText("PRICE", c.width/20*10, c.height/20*5.5);
+    ctx.fillText("OWNED", c.width/20*14, c.height/20*5.5);
 
     for (let i = 0; i<resourceNames.length; i++) {
         if (mapLocations[constellation][curMapLoc].menuNum == i) {
             ctx.fillStyle = '#FFFFFF';
-            ctx.fillRect((c.width/10)*2+55, (c.height/10)*2+115+(i*40), 90, 30);
+            ctx.fillRect(c.width/20*3.8, c.height/20*6.15+(c.height/20*1.2*i), c.width/20*3.2, c.height/20*1);
             ctx.fillStyle = '#000000'
         } else {
             ctx.fillStyle = '#FFFFFF';
         }
-        ctx.fillText(resourceNames[i], (c.width/10)*2+60, (c.height/10)*2+140+(i*40));
+        ctx.fillText(resourceNames[i], c.width/20*4, c.height/20*7+(c.height/20*1.2*i));
 
         // price
         if (station.buySell == 0) {
             // sell
             ctx.fillStyle = '#FF0000';
-            ctx.fillText(Math.floor(station.priceList[i]*0.85), (c.width/10)*2+290, (c.height/10)*2+140+(i*40));
+            ctx.fillText(Math.floor(station.priceList[i]*0.85), c.width/20*10.5, c.height/20*7+(c.height/20*1.2*i));
         } else {
             // buy
             ctx.fillStyle = '#00FF00';
-            ctx.fillText(station.priceList[i], (c.width/10)*2+290, (c.height/10)*2+140+(i*40));
+            ctx.fillText(station.priceList[i], c.width/20*10.5, c.height/20*7+(c.height/20*1.2*i));
         }
 
         // owned
         ctx.fillStyle = '#FFFFFF';
-        ctx.fillText(ship.storage[i], (c.width/10)*2+450, (c.height/10)*2+140+(i*40));
+        ctx.fillText(ship.storage[i], c.width/20*14.5, c.height/20*7+(c.height/20*1.2*i));
 
         ctx.fillStyle = '#888888';
-        ctx.fillText("ARROW KEYS SELECT - SPACE TO TRADE", (c.width/10)*2+90, (c.height/10)*2+430);
-        ctx.fillText("PRESS M TO EXIT", (c.width/10)*2+200, (c.height/10)*2+460);
+        //ctx.font = scaleFont(0.018, "Hyperspace");
+        ctx.fillText("ARROW KEYS SELECT - SPACE TO TRADE", c.width/20*5, c.height/20*17.5);
+        ctx.fillText("PRESS M TO EXIT", c.width/20*7.8, c.height/20*19.5);
 
     }
 }
